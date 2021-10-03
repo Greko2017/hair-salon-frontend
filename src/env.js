831abff839f840cc912cacbd49dba0529e66e217
Goufan A Zock, Gregory <33413355+Greko2017@users.noreply.github.com>
2021-10-03 14:55:10 +0100

    live

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // API_BASE: 'https://hairsalon-app.herokuapp.com',
-  API_BASE: 'http://localhost:8000',
+  API_BASE: 'https://hairsalon-app.herokuapp.com',
+  API_BASE_BK: 'http://localhost:8000',
 };
